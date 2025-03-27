8a2af6f8b62abb79ee36cf05ffc66547e2994df0
tangerine05 <tanvikhandeparkar5@gmail.com>
2025-03-27 10:28:49 +0530

    tanvi ammit

---
+++ frontend/proj1/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Vite + React!</h1>
+        <p>This is a simple example webpage using Vite and React.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
